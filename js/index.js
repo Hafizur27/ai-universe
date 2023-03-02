fabbb5b43b8951aaf733c4f0e83a5b97abd4523a
hafizur27 <mahmadhafiz33@gmail.com>
2023-03-02 21:15:19 +0600

    fetch url & get tools

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-console.log('find my js')
+// here we fetch our target url & get our necessary tools
+const loadTools = () =>{
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+loadTools()
