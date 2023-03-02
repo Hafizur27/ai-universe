c2ff44ae32632dbe2c7abe76fe4bbf83c9bdcc26
hafizur27 <mahmadhafiz33@gmail.com>
2023-03-03 04:36:04 +0600

    destructure tools items

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const loadTools = () =>{
     .then(data => displayTools(data.data.tools))
 }
 // declare a function for display tools details
-const displayTools = tools =>{
+const displayTools = (tools) =>{
     
     const seeMoreBtn = document.getElementById('seeMoreBtn');
 
@@ -16,12 +16,17 @@ const displayTools = tools =>{
     }
     else{
       seeMoreBtn.classList.add('d-none');
+    
     }
-
+    document.getElementById('moreToolsBtn').addEventListener('click', function(){
+      tools = tools.slice(6, 12);
+      })
+  
     const toolsContainer = document.getElementById('toolsContainer');
+    
 
     tools.forEach(tool => {
-        const {features, image, name, published_in} = tool;
+        const {features, image, name, published_in, id} = tool;
         const toolDiv = document.createElement('div')
         toolDiv.classList.add('col')
         toolDiv.innerHTML = `
@@ -29,26 +34,38 @@ const displayTools = tools =>{
         <img src="${image}" class="card-img-top" alt="">
         <div class="card-body">
           <h4 class="card-title">features</h4>
-          <h6 class="card-text text-muted">1. ${features ? features[0] : 'No Features'}</h6>
-          <h6 class="card-text text-muted">2. ${features ? features[1] : 'No Features'}</h6>
-          <h6 class="card-text text-muted">3. ${features ? features[2] : 'No Features'}</h6>
+          <h6 class="card-text text-muted">1. ${features[0] ? features[0] : 'No Features'}</h6>
+          <h6 class="card-text text-muted">2. ${features[1] ? features[1] : 'No Features'}</h6>
+          <h6 class="card-text text-muted">3. ${features[2] ? features[2] : 'No Features'}</h6>
         </div>
         <div class="card-footer d-flex justify-content-between align-items-center">
         <div class="d-flex flex-column gap-2">
             <small class= "fw-bold">${name}</small>
-            <time>${published_in}</time>
+            <time><i class="fa-regular fa-calendar-days"></i> ${published_in}</time>
             
        </div>
         <div>
-          <button class="py-1 px-3 btn bg-danger-subtle text-danger"><i class="fa-solid fa-arrow-right"></i></button>
+          <button onclick="loadToolsDetails('${id}')" class="py-1 px-3 btn bg-danger-subtle text-danger"><i class="fa-solid fa-arrow-right"></i></button>
         </div>
 
         </div>
       </div>
         `;
-        
+
         toolsContainer.appendChild(toolDiv);
     });
   
+ }
+const loadToolsDetails =  id =>{
+    const url =`https://openapi.programming-hero.com/api/ai/tool/${id}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>displayToolsDetails(data.data))
+  }
+
+const displayToolsDetails = item =>{
+  const {description, features, integrations, pricing, accuracy, input_output_examples} = item;
+  
 }
+
 loadTools()
