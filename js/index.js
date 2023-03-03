6df4d7bb75b3932b20bf1a747cc06b5552ad9019
hafizur27 <mahmadhafiz33@gmail.com>
2023-03-03 06:58:45 +0600

    modal left part

--- js/index.js
+++ js/index.js
@@ -45,7 +45,7 @@ const displayTools = (tools) =>{
             
        </div>
         <div>
-          <button onclick="loadToolsDetails('${id}')" class="py-1 px-3 btn bg-danger-subtle text-danger"><i class="fa-solid fa-arrow-right"></i></button>
+          <button onclick="loadToolsDetails('${id}')" class="py-1 px-3 btn bg-danger-subtle text-danger" data-bs-toggle="modal" data-bs-target="#toolsModal"><i class="fa-solid fa-arrow-right"></i></button>
         </div>
 
         </div>
@@ -64,7 +64,37 @@ const loadToolsDetails =  id =>{
   }
 
 const displayToolsDetails = item =>{
-  const {description, features, integrations, pricing, accuracy, input_output_examples} = item;
+  const { image_link ,description, features, integrations, pricing, accuracy, input_output_examples} = item;
+  console.log(input_output_examples)
+  const modalDetailsLeft = document.getElementById('modalBodyLeft');
+  const modalDetailsRight = document.getElementById('modalBodyRight')
+  modalDetailsLeft.innerHTML = `
+  <div class="card bg-danger-subtle p-2">
+    <p class="fw-bold"> ${description}</p>
+
+    <p class="fw-bold"> ${pricing[0].price} <span>${pricing[0].plan}</span></p>
+    <p class="fw-bold"> ${pricing[1].price} <span>${pricing[1].plan}</span></p>
+    <p class="fw-bold"> ${pricing[2].price} <span>${pricing[2].plan}</span></p>
+
+    <p>${features['1'].feature_name}</p>
+    <p>${features['2'].feature_name}</p>
+    <p>${features['3'].feature_name}</p>
+
+    <p>${integrations[0]}</p>
+    <p>${integrations[1]}</p>
+    <p>${integrations[2]}</p>
+</div>
+  
+  `;
+  modalDetailsRight.innerHTML = `
+  <div class="card">
+    <img src="${image_link[0]}" class="card-img-top" alt="">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+  </div>
+  `;
   
 }
 
